be7563f1f3116e561f7a76a026c327cfe6915e7b
Noufissa Ismaili <noufissa.ismaili.ensao@ump.ac.ma>
2025-12-22 15:47:12 +0100

    mise à jour page authentification

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from itsdangerous import URLSafeTimedSerializer
 import pymysql
 app = Flask(__name__)
 app.secret_key='votre_cle_secrete'
+serializer= URLSafeTimedSerializer(app.secret_key)
 def get_db_connection():
     return pymysql.connect(
         host="localhost",
@@ -17,6 +19,44 @@ def index():
 @app.route('/login', methods=['GET'])
 def login_page():
     return render_template('login.html')
+@app.route('/forget_password', methods=['GET', 'post'])
+def forget_password():
+    reset_link = None
+    if request.method == 'POST':
+        email = request.form.get('email')
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM utilisateurs WHERE email = %s", (email,))
+        user = cursor.fetchone()
+        conn.close()
+        if not user:
+            flash("Cet email n'existe pas")
+        else:
+            token = serializer.dumps(email, salt='reset-password')
+            reset_link = url_for('reset_password', token=token, _external=True)
+            flash("Un lien de réinitialisation du mot de passe a été envoyé à votre adresse email.")
+    return render_template('forget_password.html', reset_link=reset_link)
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    try:
+        email = serializer.loads(token, salt='reset-password', max_age=900)
+    except:
+        flash("Le lien est invalide ou a expiré.")
+        return redirect(url_for('login_page'))
+    if request.method == 'POST':
+        password= request.form.get('password')
+        confirm_password= request.form.get('confirm_password')
+        if password != confirm_password:
+            flash("Les mots de passe ne correspondent pas")
+            return redirect(url_for('reset_password', token=token))
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("UPDATE utilisateurs SET password = %s WHERE email = %s",(password, email))
+        conn.commit()
+        conn.close()
+        flash("Mot de passe modifié avec succès!")
+        return redirect(url_for('login_page'))
+    return render_template('reset_password.html', email=email)
 @app.route('/authentification', methods=['POST'])
 def authentificate():
     email = request.form.get('email')
